2b56fbc45c641e1a7a9d1ad686c0d44591db90ff
Siddharth Singh <sidsgsits@gmail.com>
2022-03-05 01:19:44 +0530

    Added the date feature and removed country selecting option

--- clockjs.js
+++ clockjs.js
@@ -1,19 +1,6 @@
-// var myVar = setInterval(myFunction, 1000);
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var country;
-// function india() {
-//   var country=1;
-//   console.log("India");
-// }
-// function usa() {
-//   var country=2;
-//   console.log("USA");
-// }
-// function uk() {
-//   var country=3;
-//   console.log("UK");
-// }
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
 var myVar;
 function myStartFunction() {
   myVar = setInterval(myFunction, 1000);
@@ -21,47 +8,25 @@ function myStartFunction() {
 }
 function myFunction() {
   let cd = new Date();
-  
             let hour = cd.getUTCHours();
             let min = cd.getUTCMinutes();
             let sec = cd.getUTCSeconds();
-  let ampm = "AM";
-  if(country==1){
-      hour+=5;
-      min+=30;
-      if(min>60){
-        min-=60;
-        hour+=1
-      }
+  var ampm = hour >= 12 ? 'PM' : 'AM';
+  hour+=5;
+  min+=30;
+  if(min>=60){
+      min =min%60;        
+      hour+=1;
+  }
       console.log("india");
-  }
-  else if(country==2){
-    hour-=7;
-    console.log("usa");
-}
-else if(country==3){
-  hour+=1;
-  console.log("uk");
-}
-
-
-  if (hour > 12) {
-    hour -= 12;
-    ampm = "PM";
-  }
-  if (hour == 0) {
-    hour = 12;
-    ampm = "AM";
-  }
+hour = hour % 12;
+hour = hour ? hour : 12; // the hour '0' should be '12'
+hour = hour < 10 ? '0'+hour : hour;
+min = min < 10 ? '0'+min : min;
+sec = sec < 10 ? '0'+sec : sec;
   let time =
     hour + ":" + min + ":" + sec + " " + ampm;
-  // let datenow = days[cd.getDay()]+", "+cd.getDate()+" "+months[cd.getMonth()]+" "+cd.getFullYear()+" ";
-
-  // document.getElementById("myclock").innerHTML = time+" "+datenow;
+  let datenow = cd.getDate()+","+days[cd.getDay()]+","+months[cd.getMonth()];
+  time = datenow +", "+ time;
   document.getElementById("myclock").innerHTML = time;
-}
-
-function myStopFunction() {
-  clearInterval(myVar);
-  console.log("Clicked Red");
 }
